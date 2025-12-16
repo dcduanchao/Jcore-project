11a26f282cb817804d3bcf540dd4919b2879dc4d
ext.duanchao5 <duanchao@xiaoneng.cn>
2025-12-16 14:46:36 +0800

    添加user

---
+++ Jcore-user/src/main/java/com/dc/JcoreUserApplication.java
@@ -0,0 +1,13 @@
+package com.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcoreUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcoreUserApplication.class, args);
+    }
+
+}
